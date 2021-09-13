5ffb262216181b1e662010515a921ad3c76493fc
中西　聡多 <89884756+Souta-Nakanishi@users.noreply.github.com>
2021-09-13 10:55:42 +0900

    Update main.js

--- assets/js-2/main.js
+++ assets/js-2/main.js
@@ -99,8 +99,8 @@ app.component('answer-input', {
   data: function () {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
-      okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      okMessage: '正解',
+      ngMessage: '�エラー� 誤答の可能性あり',
       message: '',
       inputAnswer: '',
     }
